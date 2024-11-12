08c815e80fc235631c0d89d6184d2be94df5ed03
Dogddg888 <coco.w.copo@maiL.com>
2024-11-12 11:01:01 +0100

    Add autoprefixer to fix build issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,29 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-gray-900 text-white overflow-hidden">
-      {/* Floating Icons */}
-      <div className="absolute top-4 right-4 flex space-x-4">
-        <Link href="t.me/enterVibee" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="/telegram-icon.png" // Make sure to add telegram-icon.png to the public folder
-            alt="Telegram"
-            width={40}
-            height={40}
-            className="hover:opacity-80"
-          />
+      {/* Centered and Clickable Floating Icons */}
+      <div className="absolute top-4 right-4 flex space-x-4 items-center">
+        <Link href="https://t.me/enterVibee" target="_blank" rel="noopener noreferrer" legacyBehavior>
+          <a target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/telegram-icon.png" // Make sure to add telegram-icon.png to the public folder
+              alt="Telegram"
+              width={40}
+              height={40}
+              className="hover:opacity-80"
+            />
+          </a>
         </Link>
-        <Link href="https://dexscreener.com/solana/4b6akdqcqpqaylbkmdex4k9nrszj86qdax8vapzp5ra6" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="/dexscreener-icon.png" // Make sure to add dexscreener-icon.png to the public folder
-            alt="Dexscreener"
-            width={40}
-            height={40}
-            className="hover:opacity-80"
-          />
+        <Link href="https://dexscreener.com/solana/4b6akdqcqpqaylbkmdex4k9nrszj86qdax8vapzp5ra6" target="_blank" rel="noopener noreferrer" legacyBehavior>
+          <a target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/dexscreener-icon.png" // Make sure to add dexscreener-icon.png to the public folder
+              alt="Dexscreener"
+              width={40}
+              height={40}
+              className="hover:opacity-80"
+            />
+          </a>
         </Link>
       </div>
 
